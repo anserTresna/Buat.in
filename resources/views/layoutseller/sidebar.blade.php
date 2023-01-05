b2ba523b72e5e6324b0b5af033f9cda588a78f3d
alfydije <alfiyahdjayanti39@gmail.com>
2023-01-05 11:13:53 +0700

    membenarkan

--- resources/views/layoutseller/sidebar.blade.php
+++ resources/views/layoutseller/sidebar.blade.php
@@ -53,7 +53,7 @@
                 </a>
             </li>
                   <li class="nav-item">
-                    <a href="#" class="nav-link">
+                    <a href="/logout" class="nav-link">
                       <i class="nav-icon fas fa-sign-out-alt"></i>
                       <p>
                         Sign Out
